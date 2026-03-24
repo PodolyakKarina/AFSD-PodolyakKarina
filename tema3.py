5b0a7211b9bf1b7e989dece5a2569f5f2100769e
PodolyakKarina <karinaiulia27@gmail.com>
2026-03-24 10:47:12 +0200

    Update tema3.py

--- tema3.py
+++ tema3.py
@@ -1,130 +1,146 @@
 import csv
 import json
 
-def citeste_produse_csv(fisier): #citește un fișier CSV cu produse și le pune într-un dicționar.
-    produse = {} #Creează un dicționar gol unde salvam produsele.
-    f = open(fisier, 'r') #Deschide fișierul pentru citire
-    continut = list(csv.reader(f)) #citește fiecare linie ca listă , continut devine o listă de liste
+def citeste_produse_csv(fisier):
+    produse = {}
+    f = open(fisier, 'r')
+    continut = list(csv.reader(f))
     f.close()
 
-    for linie in continut: #Parcurge fiecare linie si sare peste header
+    for linie in continut:
         if linie[0] == "id":
             continue
 
-        id_produs = linie[0] #La datele din fiecare coloană tranforma pret in nr real si stoc in nr intreg
+        id_produs = linie[0]
         nume = linie[1]
         pret = float(linie[2])
         stoc = int(linie[3])
 
-        produse[id_produs] = { # adauga produsul in dictionar
+        produse[id_produs] = {
             "nume": nume,
             "pret": pret,
             "stoc": stoc
         }
     return produse
 
-def citeste_reduceri_json(fisier): # citește JSON-ul și îl transformă automat în dictionar
+
+def citeste_reduceri_json(fisier):
     f = open(fisier, 'r')
     reduceri = json.load(f)
     f.close()
     return reduceri
+
+
 def scrie_bon_txt(fisier, text):
-    f = open(fisier, 'w') #rescrie fisierul
-    f.write(text) #pune textul în fișier
+    f = open(fisier, 'w')
+    f.write(text)
     f.close()
+
+
 produse = citeste_produse_csv("produse.csv")
 reduceri = citeste_reduceri_json("reduceri.json")
 
-produse_lista = ["espresso", "latte", "cappuccino", "ceai", "ciocolata calda", "croissant"]
-preturi = [8.0, 12.0, 11.0, 7.0, 10.0, 9.0]
-stoc = [20, 15, 18, 30, 12, 10]
-cant_comanda = [0, 0, 0, 0, 0, 0]
-
+comanda = {}   # in loc de lista
 reducere_curenta = 0
-def afisare_meniu(produse, preturi, stoc):
-    print("\nProduse disponibile:")
-    for i in range(len(produse)):
-        print(i, produse[i], preturi[i], "lei", "stoc:", stoc[i])
 
-def adaugare_produs(cant_comanda, stoc, index, cantitate):
-    if index >= 0 and index < len(stoc):
+
+def afisare_meniu(produse):
+    print("\nProduse disponibile:")
+    for id_produs in produse:
+        p = produse[id_produs]
+        print(id_produs, p["nume"], p["pret"], "lei", "stoc:", p["stoc"])
+
+
+def adaugare_produs(comanda, produse, id_produs, cantitate):
+    if id_produs in produse:
         if cantitate > 0:
-            disponibil = stoc[index] - cant_comanda[index]
-            if cantitate <= disponibil:
-                cant_comanda[index] = cant_comanda[index] + cantitate
+            stoc = produse[id_produs]["stoc"]
+            deja = comanda.get(id_produs, 0)
+
+            if cantitate <= stoc - deja:
+                comanda[id_produs] = deja + cantitate
                 print("produs adaugat")
             else:
                 print("stoc insuficient")
         else:
             print("cantitate invalida")
     else:
-        print("index gresit")
-def scadere_produs(cant_comanda, index, cantitate):
-    if index >= 0 and index < len(cant_comanda):
+        print("id invalid")
+
+
+def scadere_produs(comanda, id_produs, cantitate):
+    if id_produs in comanda:
         if cantitate > 0:
-            if cantitate <= cant_comanda[index]:
-                cant_comanda[index] = cant_comanda[index] - cantitate
-                print("produs scazut")
+            if cantitate < comanda[id_produs]:
+                comanda[id_produs] -= cantitate
             else:
-                print("nu exista atatea produse")
+                del comanda[id_produs]
+            print("produs actualizat")
         else:
             print("cantitate invalida")
     else:
-        print("index gresit")
-def calcul_total(cant_comanda, preturi):
-    total = 0
-    for i in range(len(cant_comanda)):
-        total = total + cant_comanda[i] * preturi[i]
-    return total
-def stabilire_reducere(total, tip):
-    reducere = 0
-    if tip == "student":
-        if total >= 30:
-            reducere = total * 0.10
-        else:
-            print("Total insuficient pentru student")
-    if tip == "happy":
-        if total >= 50:
-            reducere = total * 0.15
-        else:
-            print("Total insuficient pentru happy")
-    if tip == "cupon":
-        if total >= 25:
-            reducere = 7
-        else:
-            print("Total insuficient pentru cupon")
-    if reducere > total:
-        reducere = total
+        print("produsul nu este in comanda")
 
-    return reducere
-def afisare_bon(produse, preturi, cant_comanda, reducere):
+
+def calcul_total(comanda, produse):
+    total = 0
+    for id_produs in comanda:
+        total += comanda[id_produs] * produse[id_produs]["pret"]
+    return total
+
+
+def stabilire_reducere(total, tip, reduceri):
+    if tip not in reduceri:
+        return 0
+
+    r = reduceri[tip]
+
+    if total < r["prag"]:
+        print("Prag neindeplinit")
+        return 0
+
+    if r["tip"] == "procent":
+        return total * r["valoare"] / 100
+    else:
+        return r["valoare"]
+
+
+def afisare_bon(produse, comanda, reducere):
     total = 0
     text = "\nBON\n"
     print("\nBON")
-    for i in range(len(produse)):
-        if cant_comanda[i] > 0:
-            subtotal = cant_comanda[i] * preturi[i]
-            total = total + subtotal
-            print(produse[i], "x", cant_comanda[i], "=", subtotal, "lei")
-            text += produse[i] + " x " + str(cant_comanda[i]) + " = " + str(subtotal) + " lei\n"
+
+    for id_produs in comanda:
+        cant = comanda[id_produs]
+        pret = produse[id_produs]["pret"]
+        nume = produse[id_produs]["nume"]
+
+        subtotal = cant * pret
+        total += subtotal
+
+        print(nume, "x", cant, "=", subtotal, "lei")
+        text += f"{nume} x {cant} = {subtotal} lei\n"
+
     print("total:", total)
     print("reducere:", reducere)
     print("total final:", total - reducere)
 
-    text += "total: " + str(total) + "\n"
-    text += "reducere: " + str(reducere) + "\n"
-    text += "total final: " + str(total - reducere) + "\n"
+    text += f"total: {total}\n"
+    text += f"reducere: {reducere}\n"
+    text += f"total final: {total - reducere}\n"
+
     return text
 
-def finalizare_comanda(stoc, cant_comanda):
-    for i in range(len(stoc)):
-        stoc[i] = stoc[i] - cant_comanda[i]
-        cant_comanda[i] = 0
+
+def finalizare_comanda(produse, comanda):
+    for id_produs in comanda:
+        produse[id_produs]["stoc"] -= comanda[id_produs]
+    comanda.clear()
 
 
-def anulare_comanda(cant_comanda):
-    for i in range(len(cant_comanda)):
-        cant_comanda[i] = 0
+def anulare_comanda(comanda):
+    comanda.clear()
+
 while True:
     print("\nMENIU")
     print("1 afisare meniu")
@@ -134,69 +150,60 @@ while True:
     print("5 finalizare comanda")
     print("6 anulare comanda")
     print("0 iesire")
+
     optiune = input("optiune:")
+
     if optiune == "1":
-        afisare_meniu(produse_lista, preturi, stoc)
+        afisare_meniu(produse)
+
     elif optiune == "2":
-        index = int(input("index produs:"))
+        id_produs = input("id produs:")
         cant = int(input("cantitate:"))
-        adaugare_produs(cant_comanda, stoc, index, cant)
+        adaugare_produs(comanda, produse, id_produs, cant)
+
     elif optiune == "3":
-        index = int(input("index produs:"))
+        id_produs = input("id produs:")
         cant = int(input("cantitate de scazut:"))
-        scadere_produs(cant_comanda, index, cant)
+        scadere_produs(comanda, id_produs, cant)
 
     elif optiune == "4":
-
-        total = calcul_total(cant_comanda, preturi)
+        total = calcul_total(comanda, produse)
 
         if total == 0:
             print("Comanda este goala")
-
         else:
             print("1 student")
             print("2 happy")
             print("3 cupon")
             print("4 fara reducere")
-            print("5 inapoi")
 
             alegere = input("alege:")
 
             if alegere == "1":
-                reducere_curenta = stabilire_reducere(total, "student")
-
+                reducere_curenta = stabilire_reducere(total, "student", reduceri)
             elif alegere == "2":
-                reducere_curenta = stabilire_reducere(total, "happy")
-
+                reducere_curenta = stabilire_reducere(total, "happy", reduceri)
             elif alegere == "3":
-                reducere_curenta = stabilire_reducere(total, "cupon")
-
+                reducere_curenta = stabilire_reducere(total, "cupon", reduceri)
             elif alegere == "4":
                 reducere_curenta = 0
 
     elif optiune == "5":
-
-        total = calcul_total(cant_comanda, preturi)
+        total = calcul_total(comanda, produse)
 
         if total == 0:
             print("nu exista produse in comanda")
-
         else:
-
-            if reducere_curenta > total:
-                reducere_curenta = total
-
-            text_bon = afisare_bon(produse_lista, preturi, cant_comanda, reducere_curenta)
-
+            text_bon = afisare_bon(produse, comanda, reducere_curenta)
 
             scrie_bon_txt("bon.txt", text_bon)
 
-            finalizare_comanda(stoc, cant_comanda)
+            finalizare_comanda(produse, comanda)
 
             reducere_curenta = 0
 
     elif optiune == "6":
-        anulare_comanda(cant_comanda)
+        anulare_comanda(comanda)
         reducere_curenta = 0
         print("comanda anulata")
 
